181330b69ead6ff12050c37ea9f63f95549a3020
demondark100 <64336119+demondark100@users.noreply.github.com>
2022-05-15 17:12:59 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,29 @@
+// las variables son para guardar datos
+// estam let y comst :
+
+let variable1;
+let variable2;
+
+// let es una variable regionales y va cambiando
+// su valor a medida que avanza el codigo:
+
+const variable3;
+const variable4;
+
+// aqui las constantes las cuales su valor no cambiara
+// en el resto del codigo:
+
+let variable5 = "cadena";
+const variable6 = 57;
+
+// en la parte anterior solo inicializamos/declaramos una variable:
+// pero aqui ya la estamos inicializando y dandole un valor:
+
+
+// ejercicios con dodigo:
+
+let variable7 = "es una variable xdxd";
+
+const pi = 3,1416;
+
+// eso es todo con las variables:
