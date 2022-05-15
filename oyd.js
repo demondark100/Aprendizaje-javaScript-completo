80a59b29a19af620398e606d61a802ff669f72f7
demondark100 <64336119+demondark100@users.noreply.github.com>
2022-05-15 17:40:10 -0500

    operadores de asignacion y pedir datos
    aqui hice una pequeña practica con los operadores de asignacion y con prompt que es para pedir datos

---
+++ oyd.js
@@ -0,0 +1,74 @@
+// luego vienen los operadores de asignacion y 
+// pedir datos con prompt:
+
+let numero1 = 15;
+numero1 += 10;
+
+console.log(numero1);
+
+let numero2 = 50;
+numero2 -= 25;
+
+console.log(numero2);
+
+let numero3 = 65;
+numero3 *= 15;
+
+console.log(numero3);
+
+let numero4 = 15;
+numero4 /= 3;
+
+console.log(numero4);
+
+let numero5 = 48;
+numero5 %= 7;
+
+console.log(numero5);
+
+// estos son los operadores de asignacion
+// y sirve mucho:
+
+let dato = prompt("dime tu nombre");
+
+console.log(dato);
+
+// aqui hemos pedido un dato en este caso un nombre
+// el cual se guardara en la variable
+// dato:
+
+// ahora una pequeña practica:
+
+let pedirNumero1 = prompt("dime un numero para sumar");
+
+pedirNumero1 += 5;
+
+console.log(pedirNumero1);
+
+let pedirNumero2 =prompt("dime un numero para sumar");
+
+pedirNumero2 -= 5;
+
+console.log(pedirNumero2);
+
+let pedirNumero3 =prompt("dime un numero para sumar");
+
+pedirNumero2 *= 5;
+
+console.log(pedirNumero3);
+
+let pedirNumero4 =prompt("dime un numero para sumar");
+
+pedirNumero2 /= 5;
+
+console.log(pedirNumero4);
+
+let pedirNumero5 =prompt("dime un numero para sumar");
+
+pedirNumero5 %= 5;
+
+console.log(pedirNumero5);
+
+// aqui hicimos un pequeño programa el cual pide un numero 
+// y lo suma/resta/multiplica/divide/porciento el dato que 
+// nos da el usuario.
